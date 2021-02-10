8816d2da1da334a82d080e709f1ba6875cc78fa8
Mike Liu <myclues@gmail.com>
2021-02-10 15:20:41 -0500

    tryign to filter

--- js/src/index.js
+++ js/src/index.js
@@ -1,4 +1,5 @@
 import { of, Observable } from 'rxjs';
+import { filter } from 'rxjs/operators';
 import dayjs from 'dayjs';
 
 import { getQuote } from './quoteApi.js';
@@ -9,7 +10,16 @@ const config = {
 };
 
 (function main() {
-    // https://stackoverflow.com/questions/41388199/how-to-create-an-infinite-observable-that-produces-random-numbers-at-random-inte
+    let minPrice;
+    /**
+    https://rxjs.dev/api/index/class/Observable
+    - Observable constructor takes a handler (subscriber => ())
+    - subscriber has 3 methods: .next, .error, .complete to emit those events
+    - when subscribing to an instance of Observable, provide 3 handlers for what to do when receiving same 3 events
+
+    Recursive poller to generate data stream:
+    https://stackoverflow.com/questions/41388199/how-to-create-an-infinite-observable-that-produces-random-numbers-at-random-inte
+    */
     const quotePoller = new Observable(sub => {
         let timeout = null;
 
@@ -32,13 +42,21 @@ const config = {
         });
     });
 
-    quotePoller.subscribe({
-        next: ev => console.log(ev),
-        error: err => console.error(err),
-        complete: () => {
-            process.exit(0);
-        }
-    });
+    quotePoller
+        .pipe(
+            filter(val => minPrice && val < minPrice)
+        )
+        .subscribe({
+            next: ev => {
+                console.log(ev);
+                if (!minPrice) console.log('No price set yet.');
+                console.log(`Setting minPrice to: ${ev.price}`);
+            },
+            error: err => console.error(err),
+            complete: () => {
+                process.exit(0);
+            }
+        });
 
     console.log(`Polling ${config.symbol} every ${config.interval} seconds...`);
 })();
